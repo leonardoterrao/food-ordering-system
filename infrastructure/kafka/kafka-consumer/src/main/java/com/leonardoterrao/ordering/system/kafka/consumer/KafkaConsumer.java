4460bb8b5e90e0d422ad8910253033e14e671385
leonardoterrao <leonardoterrao@gmail.com>
2025-07-22 20:26:22 -0300

    kafka consumer

--- infrastructure/kafka/kafka-consumer/src/main/java/com/leonardoterrao/ordering/system/kafka/consumer/KafkaConsumer.java
+++ infrastructure/kafka/kafka-consumer/src/main/java/com/leonardoterrao/ordering/system/kafka/consumer/KafkaConsumer.java
@@ -1,4 +1,11 @@
 package com.leonardoterrao.ordering.system.kafka.consumer;
 
-public class KafkaConsum {
+import org.apache.avro.specific.SpecificRecordBase;
+
+import java.util.List;
+
+public interface KafkaConsumer<T extends SpecificRecordBase> {
+
+    void receive(List<T> messages, List<String> keys, List<Integer> partitions, List<Long> offsets);
+
 }
